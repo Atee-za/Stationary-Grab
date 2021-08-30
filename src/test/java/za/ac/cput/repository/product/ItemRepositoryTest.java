1a9b833f39c92c57d6bab41836803605bd4f3713
Athenkosi <athenkosizono@gmail.com>
2021-08-30 20:36:39 +0200

    Added Item Repository Test

--- src/test/java/za/ac/cput/repository/product/ItemRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/product/ItemRepositoryTest.java
@@ -1,11 +1,14 @@
 package za.ac.cput.repository.product;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import za.ac.cput.entity.product.Item;
 import za.ac.cput.factory.product.ItemFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class ItemRepositoryTest {
 
     private static ItemRepository repository = ItemRepository.getItemRepository();
