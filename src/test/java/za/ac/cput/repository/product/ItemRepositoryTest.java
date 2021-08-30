cce81aacb9a02e18a6855f69d8d60abb27b754b0
Athenkosi <athenkosizono@gmail.com>
2021-08-30 20:35:35 +0200

    Added Item Repository Test

---
+++ src/test/java/za/ac/cput/repository/product/ItemRepositoryTest.java
@@ -0,0 +1,41 @@
+package za.ac.cput.repository.product;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.product.Item;
+import za.ac.cput.factory.product.ItemFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    private static ItemRepository repository = ItemRepository.getItemRepository();
+    private Item book = ItemFactory.createItem("Advance SQL", "Cover all SQL skills needed in the industry",
+            "Good",856.7);
+    @Test
+    void a_create() {
+        Item created = repository.create(book);
+        assertNotNull(created);
+    }
+
+    @Test
+    void b_read() {
+        assertEquals("Advanced SQl", repository.read(book.getItemId()));
+    }
+
+    @Test
+    void c_update() {
+        Item update = new Item.Builder().copy(book).itemName("SQL Master").build();
+        Item x = repository.update(update);
+        assertEquals(x.getItemId(), book.getItemId());
+    }
+
+    @Test
+    void e_delete() {
+        assertTrue(repository.delete(book.getItemId()));
+    }
+
+    @Test
+    void d_getAll() {
+        assertEquals(1, repository.getAll().size());
+    }
+}
