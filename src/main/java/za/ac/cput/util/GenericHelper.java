cf215493da0de920c1f59dfc5acd6a98b0498ba8
Athenkosi <athenkosizono@gmail.com>
2021-08-30 19:37:13 +0200

    Added GenericHelper class

--- src/main/java/za/ac/cput/util/GenericHelper.java
+++ src/main/java/za/ac/cput/util/GenericHelper.java
@@ -7,4 +7,8 @@ public class GenericHelper {
     public static String generateId(){
         return UUID.randomUUID().toString();
     }
+
+    public static boolean isEmptyOrNull(String data){
+        return data.isEmpty() || data == null || data.equalsIgnoreCase("null");
+    }
 }
